b4c97bb9b0bdbcd7b2949738834b58d90c399070
heimanu tepu <42362243+manuts987@users.noreply.github.com>
2020-10-19 13:21:16 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -2,13 +2,15 @@
 # -*- coding: utf-8 -*-
 
 # TODO: Importez vos modules ici
-
+import math
 
 
 # TODO: Définissez vos fonction ici
-
+def Masse_volumique(a :int,b:int,c:int) -> int:
+    V = 4/3*(math.pi)*a*b*c
+    return V
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-
-    pass
+    volume = Masse_volumique(12,25,45)
+    print(volume)
